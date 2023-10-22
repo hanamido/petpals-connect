97ab5e3fba59df4261ecfda00d962dd50c5032ca
Hanami Do <hanamido@Hanamis-Air.attlocal.net>
2023-10-21 19:02:11 -0700

    added outline for routes, proposed structure for navigation/pages

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -15,19 +15,37 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>PetPals Connect</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          <b>Proposed Pages Structure:</b>
-          <li>'/': Home Page</li>
-          <li>'/pets': Page to view all available animals</li>
-          <li>'/signup': Signup Page for New Users</li>
-          <li>'/signin': Sign-in Page for current users</li>
-          <li>'/admin-signin': Sign-in page for admin (for shelters to add animals?)</li>
-          <li>'/admin-signup': Sign-up page for new admins</li>
+          <b>Proposed Structure:</b>
+          <li>'/' - Landing Page/Navigation</li>
+          <br />
+          
+          <b>Proposed Pets Structure:</b>
+          <li>'/pets' - Page to view all available animals</li>
+          <li>'/pets/:pet_id/' - Details page for one pet</li>
+          <li>'/pets/create' - Add new pets to the app</li>
+          <li>'/pets/edit/:pet_id' - Edit a pet in the app</li>
+          <br />
+
+          <b>Proposed Users Structure:</b>
+          <li>'/users/signup' - Signup Page for New Users</li>
+          <li>'/users/login' - Login Page for current users</li>
+          <li>'/users/:user_id' - Details page for one user</li>
+          <li>'/users/edit/:user_id' - Edit a user in the app</li>
+          <br />
+          
+          <b>Proposed Admin Structure:</b>
+          <li>'/admins/signup' - Signup page for new admins</li>
+          <li>'/admins/login' - Login page for admin</li>
+          <li>'/admins/:admin_id' - Details page for one admin</li>
+          <li>'/admins/edit/:admin_id' - Edit an admin</li>
+          <br />
+
         </p>
       </div>
       <p className="read-the-docs">
--- src/server/main.js
+++ src/server/main.js
@@ -3,13 +3,62 @@ const ViteExpress = require('vite-express')
 
 const app = express();
 
-app.get("/pets", (req, res) => {
-  /* Display all pets in the database. 
-   */
-  res.send("All pets in the database");
+/* PETS ROUTES */
+app.get("/pets", (err, req, res) => {
+  // Display all pets in the app 
 });
 
-app.get("/")
+app.get("/pets/:pet_id", (err, req, res) => {
+  // Display details for one pet when clicked on
+})
+
+app.post("/pets/add", (err, req, res) => {
+  // Add a new pet to the app
+  // Gets new pet info from form and adds it to database
+  // Then pass new data to frontend
+})
+
+app.put("/pets/edit/:pet_id", (err, req, res) => {
+  // Edit a pet given the pet ID
+  // Send the new pet data to frontend
+})
+
+
+/* USERS ROUTES */
+app.post("/users/signup", (err, req, res) => {
+  // Receives user info from frontend to pass to database
+})
+
+app.post("/users/login", (err, req, res) => {
+  // Receives user info from database to pass to frontend
+})
+
+app.get("/users/:user_id", (err, req, res) => {
+  // Gets info for one user (admin view?)
+})
+
+app.put("/users/edit/:user_id", (err, req, res) => {
+  // Edit a user given the user ID
+  // Send the new user details to frontend
+})
+
+
+/* ADMIN ROUTES */
+app.post("/admins/signup", (err, req, res) => {
+  // Gets info from frontend to pass to database
+})
+
+app.post("/admins/login", (err, req, res) => {
+  // Gets info from database to pass to frontend
+})
+
+app.get("/admins/:admin_id", (err, req, res) => {
+  // Gets details about one admin
+})
+
+app.put("/admins/edit/:admin_id", (err, req, res) => {
+  // Edit an admin given the admin ID
+})
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
