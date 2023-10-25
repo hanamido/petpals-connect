8a9aea6d4bc6ed02fa806b63e7fd748d7bc1117e
Alyssa Feutz <86038809+studentaly@users.noreply.github.com>
2023-10-25 12:43:21 -0700

    Added routing

---
+++ petPalsReact/src/pages/add.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../App.css";
+import bone from "../images/bone.svg";
+import user from "../images/user-regular.svg";
+
+function AddPet() {
+  return <div>Future Add Pet Page</div>;
+}
+
+export default AddPet;
