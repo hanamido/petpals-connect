f39727156707acaabec72feb9988a31bb4ecafe3
Hanami Do <hanamido@Hanamis-Air.attlocal.net>
2023-11-04 12:55:02 -0700

    added routes to view and add pets, users, and admin, and db connection

--- src/server/routes/pets.routes.js
+++ src/server/routes/pets.routes.js
@@ -15,19 +15,19 @@ petsRouter.get("/", async (req, res) => {
   // Declare query1
   let showPetsQuery;
 
-  // if there is no query string, perform a basic SELECT
-  showPetsQuery = 'SELECT * from Pets';
+  // Perform a basic select to get all animals
+  showPetsQuery = 'SELECT * from Animals';
 
-  // On Pets Card: Species, Name, Color(s), Age
-  // For More Details:  Gender, Size, Coat, Tags, Contact Info for Shelter
+  // On Pets Card: Name, Type, Picture, Availability
+  // For More Details: + Description
 
   // Run the query
-  // db.pool.query(showPetsQuery, function (err, results, fields) {
-  //   // get all the pets in the database to pass to frontend
-  //   if (err) throw err;  // error handling
-  //   // send results to frontend
-  //   res.send(results);
-  // })
+  db.pool.query(showPetsQuery, function (err, results, fields) {
+    // get all the pets in the database to pass to frontend
+    if (err) throw err;  // error handling
+    // send results to frontend
+    res.send(results);
+  })
 
   /* GETS THE DATA FROM PETFINDERS API */
   // Get the access token from the API
@@ -61,26 +61,42 @@ petsRouter.get("/search/:query", (req, res) => {
   // Display a pet based on the search parameter
 }); 
 
-petsRouter.get("/:pet_id", (req, res) => {
+petsRouter.get("/:animal_id", (req, res) => {
   // TODO: Display details for one pet when clicked on
-  // Declare query1
-  let showPetQuery;
-
-  // perform SELECT based on pet_id
-  showPetQuery = `SELECT * from Pets WHERE pet_id = ${req.params.pet_id}`;
+  // perform SELECT based on animal_id
+  let showAnimalQuery = `SELECT * from Animals WHERE animal_id = ${req.params.animal_id}`;
 
   // Run the query
-  // db.pool.query(showPetsQuery, function (err, results, fields) {
-  //   // get all the pets in the database to pass to frontend
-  //   if (err) throw err;  // error handling
-  //   // send results to frontend
-  //   res.send(results);
-  // })
+  db.pool.query(showAnimalQuery, function (err, results, fields) {
+    // get all the pets in the database to pass to frontend
+    if (err) throw err;  // error handling
+    // send results to frontend
+    res.send(results);
+  })
 })
   
 petsRouter.post("/add", (req, res) => {
   // Add a new pet to the app
   // Gets new pet info from frontend form and adds it to database
+  let data = req.body;
+  let name = data['name'];
+  let animal_type = data['animal-type'];
+  let picture = data['picture'];
+  let animal_availability = data['animal-availability'];
+  let description = data['description'];
+
+  // TODO: Figure out how to send picture as picture to display on frontend?
+
+  // Create the query and run it on the db
+  const query = `INSERT INTO Animals (${name}, ${animal_type}, ${picture}, ${animal_availability}, ${description})`;
+  db.pool.query(query, function(error, rows, fields) {
+    if (error) {
+      console.log(error);
+      res.sendStatus(400);
+    } else {
+      res.redirect('/');
+    }
+  })
 })
 
 petsRouter.put("/edit/:pet_id", (req, res) => {
--- src/server/routes/users.routes.js
+++ src/server/routes/users.routes.js
@@ -1,28 +1,53 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
+const homeRouter = require('../main');
 
 const usersRouter = express.Router();
+
+// db stuff
+const db = require('../database/db-connector');
 
 /* USERS ROUTES */
 usersRouter.post("/signup", (req, res) => {
   // Receives user info from frontend to pass to database
+  let data = req.body;
+  let first_name = data['first-name'];
+  let last_name = data['last-name'];
+  let phone_number = data['phone-number'];
+  let email = data['email'];
+  let street = data['street'];
+  let city = data['city'];
+  let state = data['state'];
+  let zip_code = data['zip-code'];
+
+  // create query and run it
+  let query = `INSERT INTO Prospective_Owners (${first_name}, ${last_name}, ${phone_number}, ${email}, ${street}, ${city}, ${state}, ${zip_code})`;
+  db.pool.query(query, function(error, rows, fields) {
+    if (error) {
+      console.log(error);
+      res.sendStatus(400);
+    } else {
+      res.redirect(homeRouter);
+    }
+  })
 })
 
-usersRouter.get("/:user_id", (req, res) => {
+usersRouter.get("/:owner_id", (req, res) => {
   // Gets info for one user from the database once they are logged in
   // Declare query1
   let showUserQuery;
 
   // Perform SELECT based on user_id
-  showUserQuery = `SELECT * from Users where user_id = ${req.params.user_id}`;
+  showUserQuery = `SELECT * from Prospective_Owners where owner_id = ${req.params.owner_id}`;
   // Run the query
-  // db.pool.query(showUserQuery, function (err, results, fields) {
-  //   // get all the users in the database to pass to frontend
-  //   if (err) throw err;  // error handling
-  //   // send results to frontend
-  //   res.send(results);
-  // })
+  db.pool.query(showUserQuery, function (err, results, fields) {
+    // get one user in the database to pass to frontend
+    if (err) throw err;  // error handling
+    // send results to frontend
+    res.send(results);
+    // res.redirect(homeRouter);
+  })
 })
 
 usersRouter.put("/edit/:user_id", (req, res) => {
