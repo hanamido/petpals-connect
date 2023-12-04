822ad5c0643012bc76701ccaddcfcad3414c717e
Hanami Do <hanamido@Hanamis-Air.attlocal.net>
2023-12-04 15:34:06 -0800

    added the auth and login route

--- src/server/main.js
+++ src/server/main.js
@@ -12,6 +12,25 @@ const corsOptions = {
   optionSuccessStatus: 200,
 }
 
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: 'https://petpals-connect.onrender.com',
+  clientID: '4IUw5SmUB0lFu7jlSJYtCMMICcB8u3VT',
+  issuerBaseURL: 'https://petpals-connect.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
 const app = express();
 
 app.use(cors(corsOptions));
