0ebb13e75adcea38710127fda0189a5ec48c6cad
Hanami Do <hanamido@Hanamis-Air.attlocal.net>
2023-12-04 23:10:11 -0800

    removed axios and fixed uri

--- petPalsReact/src/Auth0ProviderWithHistory.jsx
+++ petPalsReact/src/Auth0ProviderWithHistory.jsx
@@ -5,13 +5,12 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const Auth0ProviderWithHistory = ({ children }) => {
   const domain = 'petpals-connect.us.auth0.com';
   const clientId = 'jSr7nJ0uSiqOnaALA6FQ6oDMYEJN3KyC';
-  const uri = 'https://petpals-connect-frontend.onrender.com/';
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={uri}
+      redirectUri={window.location.origin}
     >
       {children}
     </Auth0Provider>
